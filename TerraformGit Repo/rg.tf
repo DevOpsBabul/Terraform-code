e8332723bef679c40e3e7dbb69a6f12eef52cacb
Babul kumar <babulliet@gmail.com>
2025-08-18 22:40:25 +0530

    Added the Rg

---
+++ TerraformGit Repo/rg.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.40.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id ="177b7e12-5f03-4f63-bcd1-ed6d1d776bff"
+}
+resource "azurerm_resource_group" "exampleTerraformGit" {
+  name     = "18-08-2025"
+  location = "central india"
+}
